0d38e6cdb1b141389b3cabeb95faf744cf0fc14f
Carlos Moroni Garcia <carlosmoronisud@gmail.com>
2025-06-05 23:02:40 -0300

    :sparkles: Agora vai!

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -20,18 +20,18 @@ public class SwaggerConfig {
         return new OpenAPI()
             .info(new Info()
                 .title("Projeto Blog Pessoal")
-                .description("Projeto Blog Pessoal - Generation Brasil")
+                .description("Projeto Blog Pessoal - Carlos Moroni")
                 .version("v0.0.1")
                 .license(new License()
                     .name("Generation Brasil")
                     .url("https://brazil.generation.org/"))
                 .contact(new Contact()
-                    .name("Generation Brasil")
-                    .url("https://github.com/conteudoGeneration")
-                    .email("conteudogeneration@generation.org")))
+                    .name("Carlos Moroni")
+                    .url("https://portfolio-carlosgarcias-projects.vercel.app/")
+                    .email("carlosmoronisud@gmail.com")))
             .externalDocs(new ExternalDocumentation()
                 .description("Github")
-                .url("https://github.com/conteudoGeneration/"));
+                .url("https://github.com/carlosmoronisud"));
     }
 
 
